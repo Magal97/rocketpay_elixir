2a4a6041327697fefe5590d9bb5919918d46738a
Magal97 <magal97@outlook.com>
2021-02-24 20:12:09 -0300

    first commit

---
+++ lib/rocketpay_web/router.ex
@@ -0,0 +1,23 @@
+defmodule RocketpayWeb.Router do
+  use RocketpayWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", RocketpayWeb do
+    pipe_through :api
+
+    get "/:filename", WelcomeController, :index
+  end
+
+
+  #if Mix.env() in [:dev, :test] do
+  #  import Phoenix.LiveDashboard.Router
+
+  #  scope "/" do
+  #    pipe_through [:fetch_session, :protect_from_forgery]
+  #    live_dashboard "/dashboard", metrics: RocketpayWeb.Telemetry
+  #  end
+  #end
+end
